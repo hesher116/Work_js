ff5625da97e17419bb9f26ed6c1dcbe2a5d7f029
unknown <georg116@ukr.net>
2021-04-01 22:53:59 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = prompt("asd asd 18?", "18");
+alert(`А я знаю, шо ти іван, ${answer}`)
+// console.log(typeof(answer), answer);
+
+// const answers = [];
+
+// answers[0] = prompt("name?", "");
+// answers[1] = prompt("sex?", "");
+// answers[2] = prompt("age?", "");
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
