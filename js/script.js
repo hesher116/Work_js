6bed3d7233e8eba10d1e0e072c7722a69524906d
Hesher <georg116@ukr.net>
2021-04-15 22:27:16 +0300

    firstProgram

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
+// Задание на урок:
+// 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+// 'Сколько фильмов вы уже посмотрели?'
+// 2) Создать объект personalMovieDB и в него поместить такие свойства:
+//     - count - сюда передается ответ на первый вопрос
+//     - movies - в это свойство поместить пустой объект
+//     - actors - тоже поместить пустой объект
+//     - genres - сюда поместить пустой массив
+//     - privat - в это свойство поместить boolean(логическое) значение false
+// 3) Задайте пользователю по два раза вопросы:
+//     - 'Один из последних просмотренных фильмов?'
+//     - 'На сколько оцените его?'
+// Ответы стоит поместить в отдельные переменные
+// Записать ответы в объект movies в формате: 
+//     movies: {
+//         'logan': '8.1'
+//     }
 "use strict";
 
-// alert("Hello");
-
-// const result = confirm("Are you here?");
-// console.log(result);
-
-const answer = prompt("asd asd 18?", "18");
-alert(`А я знаю, шо ти іван, ${answer}`)
+const nunberOfFilms =  +prompt("Cколько фильмов вы уже посмотрели?", "введите значение");
+// alert(`А я знаю, шо ти іван, ${answer}`)
 // console.log(typeof(answer), answer);
+
+const personalMovieD = {
+    count: nunberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const firstFilm =  prompt("Один из последних просмотренных фильмов?", "введите название"),
+      firstRating =  +prompt("На сколько оцените его?", "оценка"),
+      secondFilm =  prompt("Один из последних просмотренных фильмов?", "введите название"),
+      secondRating =  +prompt("На сколько оцените его?", "оценка");
+
+personalMovieD.movies[firstFilm] = firstRating;
+personalMovieD.movies[secondFilm] = secondRating;
+
+console.log(personalMovieD);
 
 // const answers = [];
 
